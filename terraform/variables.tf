3bb03bd53e8b0b84553db324f4b712bce7e694a6
pablo moreno <pablomoreno@pablos-MacBook-Pro.local>
2024-09-15 11:47:25 +0200

    Adding AWS Credentials binding 3

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,18 +1,3 @@
-# variable "aws_access_key" {
-#   description = "AWS Access Key"
-#   type        = string
-# }
-
-# variable "aws_secret_key" {
-#   description = "AWS Secret Key"
-#   type        = string
-# }
-
-variable "aws_account_id" {
-  description = "AWS Account ID"
-  type        = string
-}
-
 variable "aws_region" {
   description = "AWS region"
   type        = string
